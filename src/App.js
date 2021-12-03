839a0cbdfdf63dab2672e66f7de7883eb24e22b4
Hyun Kim <kmasterh@gmail.com>
2021-12-03 15:24:08 +0100

    first commit - Hook + setCounter + 3Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import Content from "./components/Content";
+// import TOC from "./components/TOC";
+// import Subject from "./components/Subject";
 
-function App() {
+
+// Use Hook
+const App = () => {
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Counter">
+        <h2>{number}</h2>
+      </div>
+      <div className="ButtonWrapper">
+        <button className="BasicButton" onClick={() => setNumber(number + 1)}>BasicButton</button>
+        <div className="TextButton"><text onClick={() => setNumber(number + 1)}>누르면 +1</text></div>
+        <img className="ImageButton" src="/image/tigger.png" alt="tigger" onClick={() => setNumber(number + 1)}/>
+      </div>
     </div>
   );
-}
+};
+
+
+
+
+
+
+// 여기는 연습하던 코드입니다. -> 주석처리
+// class App extends Component {
+//   // Render함수보다 먼저 실행되면서 초기화하고 싶을 때 -> Constructor안에 넣어준다.
+//   // State의 값 초기화
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       mode:"welcome",
+//       welcome:{title:"Welcome", desc:"Hello, React!!"},
+//       subject:{title:'WEB', sub:'World Wide Web!'},
+//       contents: [
+//         {id:1, title: 'HTML', desc:'HTML is for information.'},
+//         {id:2, title: 'CSS', desc:'CSS is for design.'},
+//         {id:3, title: 'JavaScript', desc:'JavaScript is for interaction.'},
+//       ]
+//     }
+//   }
+//   render() {
+//     var _title, _desc = null;
+//     if(this.state.mode === 'welcome') {
+//       _title = this.state.welcome.title;
+//       _desc = this.state.welcome.desc;
+//     } 
+//     else if(this.state.mode === 'read'){
+//       _title = this.state.contents[0].title;
+//       _desc = this.state.contents[0].desc;
+//     }
+//     return (
+//       <div className="App">
+//         {/* <Subject 
+//           title={this.state.subject.title}
+//           sub={this.state.subject.sub}>
+//         </Subject> */}
+
+//         <header>
+//           <h1><a href="/" onClick={function(e){
+//             // state가 바뀌면 render는 page를 reload해서 새로 그리는 작업을 한다. (별로 좋지 않겠지?)
+//             // preventDefault: page가 reload 되는 것을 막는다.
+//             e.preventDefault();
+//             // 여기서 this를 바로 부르면 Component를 부르는게 아니라 Undefined로 인식된다.
+//             // this.state.mode = 'welcome';
+//             // React에서 정한 규칙 -> 1) this를 부르고 싶으면 함수가 끝나는 지점에 bind(this)로 this를 불러올 수 있다
+//             // 2) this.setState 사용
+//             this.setState({
+//               mode:'welcome'
+//             });
+//           }.bind(this)}>{this.state.subject.title}</a></h1>
+//           {this.state.subject.sub}
+//         </header>
+
+//         <TOC data={this.state.contents}></TOC>
+//         <Content title={_title} desc={_desc}></Content>
+//       </div>
+//     )
+//   }
+// }
+
 
 export default App;
---
+++ src/components/Button/BasicButton.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BasicButton extends Component {
+    render() {
+        return (
+            <button>기본 버튼</button>
+        );
+    }
+}
+
+export default BasicButton;
